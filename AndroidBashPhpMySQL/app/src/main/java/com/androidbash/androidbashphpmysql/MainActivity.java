050a3c0172ee5af9c46644f5d8b18f96ffc5d7f2
etikos1 <35727521+etikos1@users.noreply.github.com>
2018-03-18 16:08:47 +0400

    Android app connected to db through pho
    An android app collects and stores user information in MySQL database. PHP is used to connect user interface with MySQL database. the user interface works through a converted webpage.

--- AndroidBashPhpMySQL/app/src/main/java/com/androidbash/androidbashphpmysql/MainActivity.java
+++ AndroidBashPhpMySQL/app/src/main/java/com/androidbash/androidbashphpmysql/MainActivity.java
@@ -1,108 +1,51 @@
-package com.androidbash.androidbashphpmysql;
+package com.example.leosc1.onlinemechanics;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-/**
- * Created by AndroidBash on 09/05/2016.
- */
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private List<Movie> movies;
-    private RecyclerView recyclerView;
-    private GridLayoutManager gridLayout;
-    private MoviesAdapter adapter;
-
-
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.WebView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://onlinemechanicscom.000webhostapp.com");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        movies = new ArrayList<>();
-        getMoviesFromDB(0);
-
-        gridLayout = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(gridLayout);
-
-        adapter = new MoviesAdapter(this, movies);
-        recyclerView.setAdapter(adapter);
-
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-
-                if (gridLayout.findLastCompletelyVisibleItemPosition() == movies.size() - 1) {
-                    getMoviesFromDB(movies.get(movies.size() - 1).getId());
-                }
-
-            }
-        });
-
-
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    private void getMoviesFromDB(int id) {
+        //noinspection SimplifiableIfStatement
+        {
+            return true;
+        }
 
-        AsyncTask<Integer, Void, Void> asyncTask = new AsyncTask<Integer, Void, Void>() {
-            @Override
-            protected Void doInBackground(Integer... movieIds) {
-
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
-                        .url("http://192.168.1.35/movies.php?id=" + movieIds[0])
-                        .build();
-                try {
-                    Response response = client.newCall(request).execute();
-
-                    JSONArray array = new JSONArray(response.body().string());
-
-                    for (int i = 0; i < array.length(); i++) {
-
-                        JSONObject object = array.getJSONObject(i);
-
-                        Movie movie = new Movie(object.getInt("id"), object.getString("movie_name"),
-                                object.getString("movie_image"), object.getString("movie_genre"));
-
-                        MainActivity.this.movies.add(movie);
-                    }
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                adapter.notifyDataSetChanged();
-            }
-        };
-
-        asyncTask.execute(id);
     }
-
-
 }
